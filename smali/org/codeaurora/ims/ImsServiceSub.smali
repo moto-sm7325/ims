moto-sm8250/moto_ims

ImsServiceStub: force disable auto reject mode

smali/org/codeaurora/ims/ImsServiceSub.smali
@@ -7352,9 +7352,7 @@
     iget v7, p0, Lorg/codeaurora/ims/ImsServiceSub;->mPhoneId:I
 
     .line 407
-    invoke-static {v6, v7}, Lorg/codeaurora/ims/utils/QtiImsExtUtils;->getAutoRejectMode(Landroid/content/ContentResolver;I)I
-
-    move-result v6
+    const/4 v6, 0x0
 
     const/4 v7, 0x0
 

moto-sm7325/ims

smali/org/codeaurora/ims/ImsServiceSub.smali
@@ -8340,9 +8340,7 @@
     iget v3, p0, Lorg/codeaurora/ims/ImsServiceSub;->mPhoneId:I
 
     .line 504
-    invoke-static {v2, v3}, Lorg/codeaurora/ims/utils/QtiImsExtUtils;->getAutoRejectMode(Landroid/content/ContentResolver;I)I
-
-    move-result v3
+    const/4 v3, 0x0
 
     move v6, v3
 